fab7d3c6aa0591b6143136a4511630aae6472866
Cameron Kingsley <cameron.kingsley13@gmail.com>
2022-01-04 15:39:28 -0800

    Update cors file with real frontend URL

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://react-cyberbug.herokuapp.com'
 		# TODO: Update this to correct origin before hosting!!!
 
     resource '*',
